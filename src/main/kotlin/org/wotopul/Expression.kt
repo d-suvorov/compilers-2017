129ab664d96d622cc79fe806aebfbc0b89e50b7c
Dmitry Suvorov <wotopul@gmail.com>
2017-03-11 23:46:57 +0300

    Implement an interpreter in a big-step semantics

---
+++ src/main/kotlin/org/wotopul/Expression.kt
@@ -0,0 +1,24 @@
+package org.wotopul
+
+import org.wotopul.LogicalExpr.ArithExpr
+
+fun eval(expr: ArithExpr, env: Map<String, Int>): Int? = when (expr) {
+    is ArithExpr.Const -> expr.value
+    is ArithExpr.Variable -> env[expr.name]
+
+    is ArithExpr.Addition -> evalBinary(expr.lhs, expr.rhs, env, ::eval, { x, y -> x + y })
+    is ArithExpr.Subtraction -> evalBinary(expr.lhs, expr.rhs, env, ::eval, { x, y -> x - y })
+    is ArithExpr.Multiplication -> evalBinary(expr.lhs, expr.rhs, env, ::eval, { x, y -> x * y })
+    is ArithExpr.Division -> evalBinary(expr.lhs, expr.rhs, env, ::eval, { x, y -> x / y })
+}
+
+fun <Sub, SubVal, Val> evalBinary(lhs: Sub, rhs: Sub, env: Map<String, Int>,
+                                  evalSub: (Sub, Map<String, Int>) -> SubVal?,
+                                  binOp: (SubVal, SubVal) -> Val): Val?
+{
+    val left = evalSub(lhs, env)
+    val right = evalSub(rhs, env)
+    if (left == null || right == null)
+        return null
+    return binOp(left, right)
+}
--- src/main/kotlin/org/wotopul/Interpreter.kt
+++ src/main/kotlin/org/wotopul/Interpreter.kt
@@ -1,5 +1,46 @@
 package org.wotopul
 
-fun interpret(program: Program, input: List<Int>): List<Int>? {
-    return null
+fun interpret(program: Program, input: List<Int>): List<Int>?
+    = eval(program, Configuration(input))?.output
+
+class Configuration(
+    val input: List<Int>,
+    val output: List<Int> = emptyList(),
+    val environment: Map<String, Int> = emptyMap())
+
+fun eval(program: Program, start: Configuration): Configuration? = when (program) {
+    is Program.Skip -> start
+
+    is Program.Assignment -> {
+        val value = eval(program.value, start.environment)
+        if (value != null) {
+            val name = program.variable
+            val updated = start.environment + (name to value)
+            Configuration(start.input, start.output, updated)
+        } else null
+    }
+
+    is Program.Read -> {
+        if (!start.input.isEmpty()) {
+            val inputHead = start.input.first()
+            val inputTail = start.input.subList(1, start.input.size)
+            val name = program.variable
+            val updated = start.environment + (name to inputHead)
+            Configuration(inputTail, start.output, updated)
+        } else null
+    }
+
+    is Program.Write -> {
+        val value = eval(program.value, start.environment)
+        if (value != null) {
+            Configuration(start.input, start.output + value, start.environment)
+        } else null
+    }
+
+    is Program.Sequence -> evalSequentially(program.first, program.rest, start)
+}
+
+fun evalSequentially(first: Program, second: Program, start: Configuration): Configuration? {
+    val afterFirst = eval(first, start) ?: return null
+    return eval(second, afterFirst)
 }
